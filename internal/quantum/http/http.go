b47989e2ed72f2450324999ac6f8f2b28375c9ef
Ian Dorion <madeindreams@protonmail.com>
2026-01-14 11:07:51 -0500

    Update http.go

--- internal/quantum/http/http.go
+++ internal/quantum/http/http.go
@@ -82,7 +82,7 @@ func (r *Routes) Register(api *gin.RouterGroup) {
 
 	// ---- Users ----
 	api.POST("/users/register", r.h.RegisterUser)
-	api.GET("/users/me", r.h.RetrieveUser)
+	api.POST("/users/me", r.h.RetrieveUser)
 
 	// ---- Devices ----
 	api.POST("/devices/register", r.h.RegisterDevice)
